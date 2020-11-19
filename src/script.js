3e8110bdc6d6c57ea1feb3b12027ea9729b1713e
Cloudripper <tim@verticalwild.com>
2020-11-19 14:23:33 -0800

    Movie Finder features complete

--- src/script.js
+++ src/script.js
@@ -1,23 +1,99 @@
+const checkStatus = (response) => {
+    if (response.ok) {
+        return response;
+    }
+    throw new Error('Request was either 404 or 500');
+}
+
+const json = (response) => response.json();
+
+
+const Movie = (props) => {
+    const { Title, Year, imdbID, Type, Poster } = props.movie; 
+
+    return (
+        <div>
+            <div className="row">
+                <div className="col-4 col-md-3 mb-3 pl-5 text-center">
+                    <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+                        <img src={Poster} className="img-fluid" />
+                    </a>
+                </div>
+                <div className="col-8 col-md-9 mb-3 text-left">
+                    <a className="movieList" href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+                        <h4>{Title}</h4>
+                        <p>{Type} | {Year}</p>
+                    </a>
+                </div>
+            </div>
+            <hr className="mb-4 break" />
+       </div>
+    )
+}
+
 class MovieFinder extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            searchTerm: '',
+            results: [],
+            error: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let { searchTerm } = this.state;
+        searchTerm = searchTerm.trim();
+        if (!searchTerm) {
+            return;
+        }
+        fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=516b1aec`).then(checkStatus).then(json).then((data) => {
+            if (data.Response === 'False') {
+                throw new Error(data.Error);
+            }
+            if (data.Response === 'True' && data.Search) {
+                this.setState({ results: data.Search, error: '' });
+            }
+        }).catch((error) => {
+            this.setState({ error: error.message });
+            console.log(error);
+        })
+
+        
 
     }
 
     render() {
+        const { searchTerm, results, error } = this.state;
+
         return (
             <div className="container">
                 <div className="row text-center mt-5">
-                    <div className="mt-5 mx-auto border bg-dark rounded holder">
-                        <h3 className="my-3 title">Movie Finder</h3>
-                        <form onSubmit="">
-                            <div className="my-3">
-                                <input className="border rounded bg-light" onChange="" type="text" value="title" placeholder="Movie Title" />
-                                <button className="btn btn-sm btn-primary ml-2 py-1" type="submit" value="submit">Search</button>
+                    <div className="my-auto mx-auto border bg-dark rounded">
+                        <h3 className="mt-4 title">Movie Finder</h3>
+                        <form onSubmit={this.handleSubmit} className="form-inline mb-3">
+                            <div className="my-3 mx-auto">
+                                <input className="form-control ml-4 pb-1 border rounded bg-light" onChange={this.handleChange} type="text" value={searchTerm} placeholder="Movie Title" />
+                                <button className="btn btn-sm btn-primary ml-2 mr-4 my-auto py-1" type="submit" value="submit">Search</button>
                             </div>
                         </form>
+                        {(() => {
+                            if (error) {
+                                return <div className="movieList mb-3">{error}</div>;
+                            }
+                            return results.map((movie) => {
+                                return <Movie key={movie.imdbID} movie={movie} />;
+                            })
+                        })()}
                     </div>
-
                 </div>
             </div>
 
